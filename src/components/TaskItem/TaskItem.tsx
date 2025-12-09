f0452bdf4cb5dfa044e6e4dea9ace3716d8f12ea
jjacque1 <jacksonjacque.jj@gmail.com>
2025-12-09 15:10:00 -0500

    created a component to handle status change, it assigns event.target.value to selectedValue and assign newStatus to selectedValue as TaskStaus to it follow that TaskStatus type defined inside index.ts

---
+++ src/components/TaskItem/TaskItem.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import type { TaskItemProps, TaskStatus } from "../../types";
+
+export function TaskItem({ task, onStatusChange, onDelete }: TaskItemProps) {
+    
+    function handleStatusChange(event: React.ChangeEvent<HTMLSelectElement>) {
+        const selectedValue = event.target.value;
+
+        const newStaus: TaskStatus = selectedValue as TaskStatus;
+
+        onStatusChange(task.id, newStaus);
+    }
+}
