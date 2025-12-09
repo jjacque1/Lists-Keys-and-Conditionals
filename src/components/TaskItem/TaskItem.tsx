f77eaf7f09c72187192bfffa179d25272f4347c9
jjacque1 <jacksonjacque.jj@gmail.com>
2025-12-09 16:03:25 -0500

    inside TaskItem component, i returned jsx that matched the lab assignment: header, discription, select with options, delete button, para to display priority and due date

--- src/components/TaskItem/TaskItem.tsx
+++ src/components/TaskItem/TaskItem.tsx
@@ -3,22 +3,39 @@ import React from "react";
 import type { TaskItemProps, TaskStatus } from "../../types";
 
 export function TaskItem({ task, onStatusChange, onDelete }: TaskItemProps) {
+  function handleStatusChange(event: React.ChangeEvent<HTMLSelectElement>) {
+    const selectedValue = event.target.value;
 
-    function handleStatusChange(event: React.ChangeEvent<HTMLSelectElement>) {
-        const selectedValue = event.target.value;
+    const newStaus: TaskStatus = selectedValue as TaskStatus;
 
-        const newStaus: TaskStatus = selectedValue as TaskStatus;
+    onStatusChange(task.id, newStaus);
+  }
 
-        onStatusChange(task.id, newStaus);
-    }
+  function handleDelete() {
+    onDelete(task.id);
+  }
 
-    function handleDelete() {
-        onDelete(task.id);
-    }
-
-    return (
+  return (
+    <div>
+      <div>
+        <h3>{task.title}</h3>
         <div>
-            
+          <select value={task.status} onChange={handleStatusChange}>
+            <option value="pending">Pending</option>
+            <option value="in-progress">In Progress</option>
+            <option value="completed">Completed</option>
+          </select>
+
+          <button onClick={handleDelete}>Delete</button>
         </div>
-    )
+      </div>
+
+      <p>{task.description}</p>
+
+      <p>
+        Priority: {task.priority}
+        Due: {task.dueDate}
+      </p>
+    </div>
+  );
 }
