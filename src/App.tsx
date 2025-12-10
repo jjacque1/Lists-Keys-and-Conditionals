40fdaf9c0014d2dff946e7e194366e1b286bc744
jjacque1 <jacksonjacque.jj@gmail.com>
2025-12-09 22:51:49 -0500

    trying to link everything together in App.tsx, imported every components and type

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css'
 
+import { useState } from "react";
+import { Task, TaskStatus } from "./types";
+import { TaskList } from "./components/TaskList/TaskList";
+import { TaskFilter } from "./components/TaskFilter/TaskFilter";
+
 function App() {
 
 
   return (
     <>
-    App
+    
     </>
   )
 }
