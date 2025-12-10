78387610895d2ae1922218f425df411f6b520e78
jjacque1 <jacksonjacque.jj@gmail.com>
2025-12-09 21:55:48 -0500

    added a handleStatusChange component that grabs event.target.value and stores it in selectedValue, have a variable called newStatus that follows the TaskStatus type imported fromn index.ts, have if statement checking if the selectedValue is empty which sets it to undefined, else newStatus is the selected value as TaskStuts type, Have a setter for onFilterChange that accepts status as newStatus and priority as selectedPriority

--- src/components/TaskFiller/TaskFilter.tsx
+++ src/components/TaskFiller/TaskFilter.tsx
@@ -1,5 +1,64 @@
 import { useState } from "react";
 
-import { TaskFilterProps, TaskStatus } from "../../types";
+import type { TaskFilterProps, TaskStatus } from "../../types";
 
-export function TaskFilter()
+export function TaskFilter(props: TaskFilterProps) {
+
+    const onFilterChange = props.onFilterChange;
+
+
+    const [selectedStatus, setSelectedStatus] = useState<TaskStatus | undefined>(undefined);
+
+
+    const[seletedPriority, setSeletedPriority] = useState<"low" | "medium" | "high" | undefined>(undefined);
+
+
+
+    function handleStatusChange(event: React.ChangeEvent<HTMLSelectElement>) {
+         const selectedValue = event.target.value;
+
+         let newStatus: TaskStatus | undefined;
+
+         if(selectedValue === " " ) { 
+            newStatus = undefined;
+         } else {
+            newStatus = selectedValue as TaskStatus; 
+         }
+
+         onFilterChange({
+            status: newStatus,
+            priority: seletedPriority,
+         })
+    }
+
+
+ function handlePriorityChange(event: React.ChangeEvent<HTMLSelectElement>) {
+    const selectedValue = event.target.value; 
+
+    let newPriority: "low" | "medium" | "high"  | undefined;
+
+    if(selectedValue === " ") {
+        newPriority = undefined;
+    } else {
+        newPriority = selectedValue as "low" | "medium" | "high";
+    }
+
+
+    setSeletedPriority(newPriority);
+
+
+    
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+}
